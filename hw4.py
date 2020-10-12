0b2b9b1282644d331f0c6ba05b0989c9141adea4
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2020-10-11 21:49:12 -0400

    Create hw4.py
    first commit

---
+++ hw4.py
@@ -0,0 +1,32 @@
+#CS 532 Assignment 4
+
+from scipy.io import loadmat
+import numpy as np
+
+#Part A
+
+
+#Least Squares Classifier Training. w = (X^T X)^(-1)X^T y
+def findweights():
+    data = loadmat('face_emotion_data.mat')
+    data_x_points = data['X']
+    data_y_points = data['y']
+    weights = np.linalg.inv(data_x_points.transpose()@data_x_points)@data_x_points.transpose()@data_y_points
+    return weights
+
+print(findweights())
+
+
+
+#Part B
+
+
+#There are 9 features in the corresponding feature vector to help decide on facial expression sentiments. The
+#feature vector is represented the following way:
+
+#feature = [x1 x2 x3 x4 x5 x6 x7 x8 x9]
+
+#To find a solution to this particular problem, I must perform the following:
+
+
+
